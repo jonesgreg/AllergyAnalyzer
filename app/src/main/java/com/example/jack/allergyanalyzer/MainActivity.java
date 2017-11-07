eef225487ef4b168f435d1322668755821053010
Jack Coleman <jc648819@sju.edu>
2017-11-07 02:29:08 -0500

    Creation of local repo

---
+++ app/src/main/java/com/example/jack/allergyanalyzer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jack.allergyanalyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
