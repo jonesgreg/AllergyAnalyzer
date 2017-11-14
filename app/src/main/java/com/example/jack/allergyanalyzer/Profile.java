03e7056cd13a8edfe14e5be149f18b1fc52efa0b
Justin Branco <jb641860@sju.edu>
2017-11-13 22:44:53 -0500

    Updated profile page

--- app/src/main/java/com/example/jack/allergyanalyzer/Profile.java
+++ app/src/main/java/com/example/jack/allergyanalyzer/Profile.java
@@ -11,50 +11,43 @@ import android.widget.EditText;
 
 public class Profile extends AppCompatActivity {
 
-    public static final String SAVED_PROFILE = "com.allergyanalyzer.allergyanalyzer";
-    public static final String SAVED_PROFILE2 = "com.allergyanalyzer.allergyanalyzer";
-    public static final String SAVED_PROFILE3 = "com.allergyanalyzer.allergyanalyzer";
-    public static final String SAVED_PROFILE4 = "com.allergyanalyzer.allergyanalyzer";
-
-         //JUSTINS CODE STARTS HERE
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_profile);
-        }
-        
-        /** Called when the user taps the Save button */
-        public void saveMessage(View view) {
-            // Do something in response to button
-            Intent intent = new Intent(Profile.this, SavedProfilePage.class);
-            //Takes in person's name
-            EditText editText = findViewById(R.id.editText5);
-            String nameMessage = editText.getText().toString();
-            intent.putExtra(SAVED_PROFILE, nameMessage);
-            startActivity(intent);
-            
-            Intent intent2 = new Intent(Profile.this, SavedProfilePage.class);
-            //Takes in person's phone number
-            EditText editText2 = findViewById(R.id.editText6);
-            String phoneMessage = editText2.getText().toString();
-            intent2.putExtra(SAVED_PROFILE2, phoneMessage);
-            startActivity(intent2);
-            
-            Intent intent3 = new Intent(Profile.this, SavedProfilePage.class);
-            //Takes in person's email
-            EditText editText3 = findViewById(R.id.editText7);
-            String emailMessage = editText3.getText().toString();
-            intent3.putExtra(SAVED_PROFILE3, emailMessage);
-            startActivity(intent3);
-            
-            Intent intent4 = new Intent(Profile.this, SavedProfilePage.class);
-            //Take's in person's allergies
-            //Batool will change this
-            EditText editText4 = findViewById(R.id.editText9);
-            String allergyMessage = editText4.getText().toString();
-            intent4.putExtra(SAVED_PROFILE4, allergyMessage);
-            startActivity(intent4);
-        }
+    public static final String SAVED_NAME = "com.allergyanalyzer.allergyanalyzer.nameMessage";
+    public static final String SAVED_PHONE = "com.allergyanalyzer.allergyanalyzer.phoneMessage";
+    public static final String SAVED_EMAIL = "com.allergyanalyzer.allergyanalyzer.emailMessage";
+    public static final String SAVED_ALLERGY = "com.allergyanalyzer.allergyanalyzer.allergyMessage";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
     }
-
+    
+    /** Called when the user taps the Save button */
+    public void saveMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(ProfilePage.this, SavedProfilePage.class);
+        Bundle extras = new Bundle();
+        //Takes in person's name
+        EditText editText = findViewById(R.id.editText5);
+        String nameMessage = editText.getText().toString();
+        extras.putString(SAVED_NAME, nameMessage);
+        
+        //Takes in person's phone number
+        editText = findViewById(R.id.editText6);
+        String phoneMessage = editText.getText().toString();
+        extras.putString(SAVED_PHONE, phoneMessage);
+        
+        //Takes in person's email
+        editText = findViewById(R.id.editText7);
+        String emailMessage = editText.getText().toString();
+        extras.putString(SAVED_EMAIL, emailMessage);
+        
+        //Take's in person's allergies
+        //Batool will change this
+        editText = findViewById(R.id.editText9);
+        String allergyMessage = editText.getText().toString();
+        extras.putString(SAVED_ALLERGY, allergyMessage);
+        intent.putExtras(extras);
+        startActivity(intent);
+    }
 }
