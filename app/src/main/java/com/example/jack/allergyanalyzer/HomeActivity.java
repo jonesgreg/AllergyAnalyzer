914bf8ad4b9bde25f8964e281b3b2025677807bf
Jack Coleman <jc648819@sju.edu>
2017-12-11 16:25:06 -0500

    simple change to where the profile button leads to

--- app/src/main/java/com/example/jack/allergyanalyzer/HomeActivity.java
+++ app/src/main/java/com/example/jack/allergyanalyzer/HomeActivity.java
@@ -62,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent i = new Intent(getApplicationContext(),ProfileActivity.class);
+                Intent i = new Intent(getApplicationContext(),SavedProfileActivity.class);
                 startActivity(i);
             }
         });
